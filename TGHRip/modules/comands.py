0ae97d8d9bffbcc7231a88272ed7714fa540710d
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-07-22 08:42:44 +0530

    Update comands.py

--- TGHRip/modules/comands.py
+++ TGHRip/modules/comands.py
@@ -1,4 +1,11 @@
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram import Client, filters
+from asyncio import TimeoutError
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery, ForceReply
+from TGHRip.modules import Translation
+from pyrogram import Client, filters 
+
 
 COMMANDS_TEXT = """
 📖 <b>TGHRip Commands</b>
@@ -33,3 +40,25 @@ COMMANDS_BUTTONS = InlineKeyboardMarkup(
         [InlineKeyboardButton("⛔️ Close", callback_data="close")]
     ]
 )
+
+
+
+@Client.on_message(filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    if not update.from_user:
+        return await update.reply_text("I don't know about you sar :(")
+    await add_user_to_database(bot, update)
+    await bot.send_message(
+        Config.LOG_CHANNEL,
+           f"#NEW_USER: \n\nNew User [{update.from_user.first_name}](tg://user?id={update.from_user.id}) started @{Config.BOT_USERNAME} !!"
+    )
+    
+    if Config.UPDATES_CHANNEL:
+      fsub = await handle_force_subscribe(bot, update)
+      if fsub == 400:
+        return
+    await update.reply_text(
+        text=Translation.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Translation.START_BUTTONS
+    )
