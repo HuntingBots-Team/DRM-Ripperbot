15f34b5485124a223b8362de158700dd71340a37
HuntingBots-Team <89862218+HuntingBots-Team@users.noreply.github.com>
2025-07-23 00:48:47 +0530

    Update userdb.py

--- TGHRip/utils/userdb.py
+++ TGHRip/utils/userdb.py
@@ -2,7 +2,20 @@ from pyrogram import Client
 from TGHRip.utils.mongodb import db
 from pyrogram.types import Message
 
+async def add_user(bot: Client, message: Message):
+    user_id = message.from_user.id
+    first_name = message.from_user.first_name
+    username = message.from_user.username
+    is_bot = message.from_user.is_bot
+    joined_at = message.date
 
-async def AddUser(bot: Client, update: Message):
-    if not await db.is_user_exist(update.from_user.id):
-           await db.add_user(update.from_user.id)
+    # Check if user exists
+    if not await db.is_user_exist(user_id):
+        # Add user with detailed info
+        await db.add_user({
+            "_id": user_id,
+            "first_name": first_name,
+            "username": username,
+            "is_bot": is_bot,
+            "joined_at": joined_at
+        })
